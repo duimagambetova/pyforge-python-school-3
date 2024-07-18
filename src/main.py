47425f30517412b5c996b757b15680ecab63e314
Алина Дуймагамбетова <duimagambetovaa@MacBook-Air-Alina.local>
2024-07-18 21:06:49 +0500

    init

--- src/main.py
+++ src/main.py
@@ -1,2 +1,16 @@
 def substructure_search(mols, mol):
-    pass
+    # Convertation of substructure SMILES to RDKit molecule object
+    substructure = Chem.MolFromSmiles(substructure_smiles)
+    
+    # List to store matched molecules
+    matched_molecules = []
+    
+    # Iterate through each SMILES string in smiles_list
+    for smiles in smiles_list:
+        molecule = Chem.MolFromSmiles(smiles)
+        
+        # Perform substructure search
+        if molecule.HasSubstructMatch(substructure):
+            matched_molecules.append(smiles)
+    
+    return matched_molecules
